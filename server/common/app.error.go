08474b31aed2957a30296f0e03f0ade7a3415d1c
Ngô Mạnh Hùng <sundayy.dev@gmail.com>
2024-11-18 20:07:37 +0700

    Add app.error.go

---
+++ server/common/app.error.go
@@ -0,0 +1,179 @@
+package common
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+type AppError struct {
+	StatusCode int    `json:"status_code"`
+	RootErr    error  `json:"-"`
+	Message    string `json:"message"`
+	Log        string `json:"log"`
+	Key        string `json:"error_key"`
+}
+
+func NewErrorResponse(root error, msg, log, key string) *AppError {
+	return &AppError{
+		StatusCode: http.StatusBadRequest,
+		RootErr:    root,
+		Message:    msg,
+		Log:        log,
+		Key:        key,
+	}
+}
+
+func NewFullErrorResponse(statusCode int, root error, msg, log, key string) *AppError {
+	return &AppError{
+		StatusCode: statusCode,
+		RootErr:    root,
+		Message:    msg,
+		Log:        log,
+		Key:        key,
+	}
+}
+
+func NewUnauthorized(root error, msg, log, key string) *AppError {
+	return &AppError{
+		StatusCode: http.StatusUnauthorized,
+		RootErr:    root,
+		Message:    msg,
+		Log:        log,
+		Key:        key,
+	}
+}
+
+func NewResourceNotFound(entity string) *AppError {
+	return &AppError{
+		StatusCode: http.StatusNotFound,
+		RootErr:    RecordNotFound,
+		Message:    fmt.Sprintf("%s not found", entity),
+		Log:        RecordNotFound.Error(),
+		Key:        "RESOURCE_NOT_FOUND",
+	}
+}
+
+func NewCustomError(root error, msg string, key string) *AppError {
+	if root != nil {
+		return NewErrorResponse(root, msg, root.Error(), key)
+	}
+	return NewErrorResponse(
+		errors.New(msg),
+		msg,
+		msg,
+		key)
+}
+
+func (e *AppError) RootError() error {
+	var err *AppError
+	if errors.As(e.RootErr, &err) {
+		return err.RootError()
+	}
+	return e.RootErr
+}
+
+func (e *AppError) Error() string {
+	return e.RootError().Error()
+}
+
+func ErrDb(err error) *AppError {
+	return NewFullErrorResponse(
+		http.StatusInternalServerError,
+		err,
+		"Something went wrong with DB",
+		err.Error(),
+		"DB_ERROR")
+}
+
+func ErrInvalidRequest(err error) *AppError {
+	return NewErrorResponse(err, "Invalid request", err.Error(), "INVALID_REQUEST")
+}
+
+func ErrInternal(err error) *AppError {
+	return NewFullErrorResponse(http.StatusInternalServerError, err, "something went wrong in the server", err.Error(), "INTERNAL_ERROR")
+}
+
+func ErrCannotListEntity(entity string, err error) *AppError {
+	return NewCustomError(
+		err,
+		fmt.Sprintf("Cannot list %s", strings.ToLower(entity)),
+		fmt.Sprintf("CANNOT_LIST_%s", strings.ToUpper(entity)))
+}
+
+func ErrCannotDeleteEntity(entity string, err error) *AppError {
+	return NewCustomError(
+		err,
+		fmt.Sprintf("Cannot delete %s", strings.ToLower(entity)),
+		fmt.Sprintf("CANNOT_DELETE_%s", strings.ToUpper(entity)))
+}
+
+func ErrEntityDeleted(entity string, err error) *AppError {
+	return NewCustomError(
+		err,
+		fmt.Sprintf("Entity %s has been deleted", strings.ToLower(entity)),
+		fmt.Sprintf("ENTITY_DELETED_%s", strings.ToUpper(entity)))
+}
+
+func ErrEntityNotFound(entity string, err error) *AppError {
+	return NewCustomError(
+		err,
+		fmt.Sprintf("Entity %s not found", strings.ToLower(entity)),
+		fmt.Sprintf("ENTITY_NOT_FOUND_%s", strings.ToUpper(entity)))
+}
+
+func ErrCannotCreateEntity(entity string, err error) *AppError {
+	return NewCustomError(
+		err,
+		fmt.Sprintf("Cannot create %s", strings.ToLower(entity)),
+		fmt.Sprintf("CANNOT_CREATE_%s", strings.ToUpper(entity)))
+}
+
+func ErrNoPermission(err error) *AppError {
+	return NewCustomError(
+		err,
+		"You have no permission",
+		"NO_PERMISSION")
+}
+
+func ErrCannotUpdateEntity(entity string, err error) *AppError {
+	return NewCustomError(
+		err,
+		fmt.Sprintf("Cannot update %s", strings.ToLower(entity)),
+		fmt.Sprintf("CANNOT_UPDATE_%s", strings.ToUpper(entity)))
+}
+
+func ErrCannotPublishMessage(topic string, err error) *AppError {
+	return NewCustomError(
+		err,
+		fmt.Sprintf("Cannot publish message to %s", topic),
+		fmt.Sprintf("CANNOT_PUBLISH_%s", strings.ToUpper(topic)))
+}
+
+func ErrTooManyRequest(api string, err error) *AppError {
+	return NewFullErrorResponse(
+		http.StatusTooManyRequests,
+		err,
+		fmt.Sprintf("Too many requests to %s", api),
+		err.Error(),
+		"TOO_MANY_REQUEST")
+}
+
+func ErrToCacheEntity(entity string, err error) *AppError {
+	return NewCustomError(
+		err,
+		fmt.Sprintf("Cannot cache %s", strings.ToLower(entity)),
+		fmt.Sprintf("CANNOT_CACHE_%s", strings.ToUpper(entity)))
+}
+
+func ErrNoCacheEntity(entity string, err error) *AppError {
+	return NewCustomError(
+		err,
+		fmt.Sprintf("Cannot get %s from cache", strings.ToLower(entity)),
+		fmt.Sprintf("NO_CACHE_%s", strings.ToUpper(entity)))
+}
+
+var DocumentNotFound = errors.New("document not found")
+var RecordNotFound = errors.New("record not found")
+var RateLimited = errors.New("rate limited")
